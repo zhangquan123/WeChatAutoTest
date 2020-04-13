17a55258b93704f60ff4c5d427cb7e900a2b7714
554413368@qq.com <554413368@qq.com>
2020-04-13 17:29:55 +0800

    zhangquan

---
+++ src/main/java/selenium/WeChatAutoTest/App.java
@@ -0,0 +1,13 @@
+package selenium.WeChatAutoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/selenium/WeChatAutoTest/AppTest.java
@@ -0,0 +1,32 @@
+package selenium.WeChatAutoTest;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * jenkins启动TestNG.xml
+     */
+
+	public static void main(String[] args) throws InterruptedException {
+		TestNG testNG = new TestNG();
+        List<String> suites = new ArrayList<String>();      
+        suites.add("C:\\Users\\Administrator\\Desktop\\WeChatBroadband\\src\\test\\java\\TestNG.xml");
+        //suites.add(".\\test-output\\testng-failed.xml");
+        testNG.setTestSuites(suites);
+        testNG.run();
+
+        /*// 等待执行结束，然后去执行失败用例
+        TestNG testNG1 = new TestNG();
+        List<String> suites1 = new ArrayList<String>();
+        Thread.sleep(5000);
+        suites1.add(".\\test-output\\testng-failed.xml");
+        testNG1.setTestSuites(suites1);
+        testNG1.run();*/
+	}
+}
